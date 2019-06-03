08aa9e056f568d045f3366c6967666ab3cdd5b46
Gurkirat Singh <tbhaxor@gmail.com>
2019-06-03 13:28:43 +0530

    added raw query executor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { spawn } from 'child_process';
 import { join } from 'path';
+
 export class OSQuery {
   private path: string;
 
@@ -22,5 +24,48 @@ export class OSQuery {
       // add the explicit path by user
       this.path = path;
     }
+  }
+
+  /**
+   * Executes the raw sql query
+   *
+   * @param {string} sql
+   * @returns {Promise<object>}
+   * @memberof OSQuery
+   */
+  public query(sql: string): Promise<object> {
+    // returning the promise
+    return new Promise<object>((res, rej) => {
+      // initializing the variables for data
+      let data: string = '';
+      let error: string = '';
+
+      // creating an instance of
+      const command = spawn(this.path, ['--json', sql]);
+
+      // on success response
+      command.stdout.on('data', (chunk: Buffer) => {
+        // add data chunk to data
+        data += chunk.toString();
+      });
+
+      // on error response
+      command.stderr.on('data', (chunk: Buffer) => {
+        error += chunk.toString();
+      });
+
+      // fired when command is exit
+      command.on('exit', (code) => {
+        // if exit successfully
+        if (code === 0) {
+          // resolve the promise
+          // tslint:disable-next-line: no-unsafe-any
+          res({ success: true, data: JSON.parse(data) });
+        } else {
+          // reject the promise
+          rej({ success: false, data: error });
+        }
+      });
+    });
   }
 }
